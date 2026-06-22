80918c83bfb754dda0a5216e248b3410fdd9091f
PETER MUHUMUKI <114877453+peterkahumu@users.noreply.github.com>
2026-06-22 19:05:39 +0300

    Revert "Auto PR: feature/server-update"

--- next.config.ts
+++ next.config.ts
@@ -13,19 +13,6 @@ const nextConfig: NextConfig = {
   env: {
     STREAMING_PROVIDER: process.env.STREAMING_PROVIDER,
   },
-  async headers() {
-    return [
-      {
-        source: '/watch/:path*',
-        headers: [
-          {
-            key: 'Permissions-Policy',
-            value: 'fullscreen=*, autoplay=*, picture-in-picture=*',
-          },
-        ],
-      },
-    ]
-  },
 }
 
 export default nextConfig
