d300ffbb9c35d3ef1406b21646647bf2ec871442
Peter Muhumuki <muhumukip@gmail.com>
2026-06-29 14:45:26 +0300

    feat: make fullscreen orientation lock global so trailers also rotate to landscape

--- components/CapacitorInit.tsx
+++ components/CapacitorInit.tsx
@@ -29,8 +29,49 @@ export default function CapacitorInit() {
         }
       })
 
+      // Handle global fullscreen events (e.g. video player or trailers)
+      let immersiveInterval: NodeJS.Timeout | null = null
+      const handleFullscreenChange = async () => {
+        try {
+          if (document.fullscreenElement) {
+            setTimeout(async () => {
+              await import('@capacitor/screen-orientation').then(m => m.ScreenOrientation.lock({ orientation: 'landscape' })).catch(() => {})
+              await StatusBar.setOverlaysWebView({ overlay: true }).catch(() => {})
+              await StatusBar.hide().catch(() => {})
+
+              immersiveInterval = setInterval(() => {
+                if (document.fullscreenElement) {
+                  StatusBar.hide().catch(() => {})
+                }
+              }, 2500)
+            }, 300)
+          } else {
+            setTimeout(async () => {
+              await import('@capacitor/screen-orientation').then(m => m.ScreenOrientation.unlock()).catch(() => {})
+              await StatusBar.setOverlaysWebView({ overlay: false }).catch(() => {})
+              await StatusBar.show().catch(() => {})
+              if (immersiveInterval) {
+                clearInterval(immersiveInterval)
+                immersiveInterval = null
+              }
+            }, 300)
+          }
+        } catch (error) {
+          console.error('Failed to change screen orientation:', error)
+        }
+      }
+      
+      document.addEventListener('fullscreenchange', handleFullscreenChange)
+
       return () => {
         backListener.then(l => l.remove()).catch(() => {})
+        document.removeEventListener('fullscreenchange', handleFullscreenChange)
+        if (immersiveInterval) clearInterval(immersiveInterval)
+        
+        // Failsafe on global unmount (though this rarely unmounts)
+        import('@capacitor/screen-orientation').then(m => m.ScreenOrientation.unlock()).catch(() => {})
+        StatusBar.setOverlaysWebView({ overlay: false }).catch(() => {})
+        StatusBar.show().catch(() => {})
       }
     }
   }, [router])
