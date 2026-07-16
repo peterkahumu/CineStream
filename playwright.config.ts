0496ae30ed811ac74e2ca775cdb3e9dc71647650
Peter Muhumuki <muhumukip@gmail.com>
2026-07-16 17:46:36 +0300

    chore: restore html reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'list',
+  reporter: 'html',
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'off',
