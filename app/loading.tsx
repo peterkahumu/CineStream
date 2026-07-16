960e46ca40cfead06ee45a7d82ec8a56c029b15c
Peter Muhumuki <muhumukip@gmail.com>
2026-07-16 16:39:06 +0300

    refactor: unify route loaders into a single global loading component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,16 +1,5 @@
-import HeroBanner from '@/components/HeroBanner'
-import MediaRow from '@/components/MediaRow'
-import styles from './page.module.css'
+import Loading from '@/components/Loading'
 
-export default function HomeLoading() {
-  return (
-    <main>
-      <HeroBanner items={[]} loading={true} />
-      <div className={`page-container ${styles.sections}`}>
-        {['t1', 't2', 't3', 't4'].map(k => (
-          <MediaRow key={k} title="Loading…" emoji="⏳" items={[]} loading={true} seeAllHref="#" />
-        ))}
-      </div>
-    </main>
-  )
+export default function GlobalLoading() {
+  return <Loading />
 }
