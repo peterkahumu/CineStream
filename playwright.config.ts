7ad44a3b5d3ce7ee5aa101ddd3c2f4a3960a4745
Peter Muhumuki <muhumukip@gmail.com>
2026-07-16 17:53:26 +0300

    chore: limit playwright to 1 worker to fix next.js cpu hang

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,10 +10,10 @@ export default defineConfig({
   expect: {
     timeout: 10000
   },
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: 1,
   reporter: [['list'], ['html']],
   use: {
     baseURL: 'http://localhost:3000',
